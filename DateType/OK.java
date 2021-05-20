7c65fc5940a0ed5b99994bfc2faacff2c9f0c396
Mato <72716024+we-insub@users.noreply.github.com>
2021-05-20 17:19:43 +0900

    생활코딩업데이트 파라미터와 아규스

--- DateType/OK.java
+++ DateType/OK.java
@@ -7,17 +7,19 @@ import org.opentutorials.iot.Lighting;
 
 public class OK {
 	
-
+	// Paramter, 매게변수 Run Configuration 에서 아규먼트에 값을 집어넣었음 args 에 값이들어옴
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
 //		String id = "화곡동 1053-10" ; 이렇게 해도되지만 매번id 값 변경귀찮으니까
 //		id 값을 인풋으로 입력받는다면?
-		String id =JOptionPane.showInputDialog("Enter a ID");
-		String bright = JOptionPane.showInputDialog("Enter a bright");
+//		String id =JOptionPane.showInputDialog("Enter a ID");
+//		String bright = JOptionPane.showInputDialog("Enter a bright");
 //		위에 값은 스트링형이고  36 번라인의 데이터 형식은 더블형이다 캐싱해야함
 //		스프링 - 더블 
-		
+		String id = args[0]; // 아규값먹인것을 배열로 값을 자동입력 JAVA APT 507 
+		String bright = args[1]; // 15.5
+		// args 설정할때 ' 따옴표가아닌 쌍따옴표로 묶어야 함 "
 		
 		// Elevator Call
 		Elevator myElevator = new Elevator(id);
